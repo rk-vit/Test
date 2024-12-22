85fa6fcda38554a547f6eed6c36502a3fb810686
Revanth Kanna M <revanthkannam05@gmail.com>
2024-12-22 21:23:02 +0530

    Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [Notes,setNotes] = useState([]);
+  const addNote = (newNoteTitle,newNoteText)=>{
+    const newNote = {title:newNoteTitle,text:newNoteText}
+    setNotes((prevValue)=>{
+      return [...prevValue,newNote]
+    })
+  }
+  const deleteNote = (key)=>{
+    setNotes((prevValue)=>{
+      return prevValue.filter((item,index)=>{
+        return index!==key;
+      })
+    })
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header />
+      <CreateArea addNote={addNote}/>
+      {Notes.map((note,index) => {
+        return <Note key={index} val={index} title={note.title} content={note.text} deleteNote={deleteNote}/>
+      })}
+      
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/CreateArea.jsx
@@ -0,0 +1,22 @@
+import {React,useState} from "react";
+
+function CreateArea(props) {
+  const[title,setTitle] = useState("");
+  const[text,setText] = useState("");
+  return (
+    <div>
+      <form>
+        <input name="title" onChange={(event)=>{setTitle(event.target.value)}} value={title}placeholder="Title" />
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={(event)=>{setText(event.target.value)}} value={text}/>
+        <button onClick={(event)=>{
+          if(title.length!=0 && text.length!=0) props.addNote(title,text); 
+          event.preventDefault();
+          setTitle(""); 
+          setText("");
+          }}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
