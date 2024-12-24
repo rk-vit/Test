9c7e1a8eedee2855239b46e6b23d93c0a8669c8c
Revanth Kanna M <revanthkannam05@gmail.com>
2024-12-24 19:21:59 +0530

    route change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,51 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
 
+import axios from "axios"
 function App() {
   const [Notes,setNotes] = useState([]);
-  const addNote = (newNoteTitle,newNoteText)=>{
-    const newNote = {title:newNoteTitle,text:newNoteText}
-    setNotes((prevValue)=>{
-      return [...prevValue,newNote]
-    })
+  const fetchNotes = async ()=>{
+    console.log("Hey")
+    try{
+      const response = await axios.get("https://noter-app-server.vercel.app/api");
+      setNotes(response.data);
+    }catch(err){
+      console.error('Error fetching Notes:',err);
+    }
   }
-  const deleteNote = (key)=>{
-    setNotes((prevValue)=>{
-      return prevValue.filter((item,index)=>{
-        return index!==key;
-      })
-    })
+  useEffect(()=>{
+    fetchNotes();
+  },[]);
+
+
+  const addNote = async (newNoteTitle,newNoteText)=>{
+    const newNote = {title:newNoteTitle,text:newNoteText,id:Notes.length}
+    try{
+      const res = await axios.post("https://noter-app-server.vercel.app/api",newNote);
+      console.log("Now work bro pls");
+      await fetchNotes();
+    }catch(err){
+        console.error("Err: ",err);
+    }
+  }
+  const deleteNote = async (key)=>{
+    try{
+      const response = await axios.delete("https://noter-app-server.vercel.app/api",{data: {key}});
+      console.log("Delete also work bro pls");
+      await fetchNotes();
+    }catch(err){
+      console.error("Error deleting notes");
+    }
   }
   return (
     <div>
       <Header />
       <CreateArea addNote={addNote}/>
       {Notes.map((note,index) => {
-        return <Note key={index} val={index} title={note.title} content={note.text} deleteNote={deleteNote}/>
+        return <Note key={index} val={note.id} title={note.title} content={note.text} deleteNote={deleteNote}/>
       })}
       
       <Footer />
--- client/src/components/CreateArea.jsx
+++ client/src/components/CreateArea.jsx
@@ -9,7 +9,7 @@ function CreateArea(props) {
         <input name="title" onChange={(event)=>{setTitle(event.target.value)}} value={title}placeholder="Title" />
         <textarea name="content" placeholder="Take a note..." rows="3" onChange={(event)=>{setText(event.target.value)}} value={text}/>
         <button onClick={(event)=>{
-          if(title.length!=0 && text.length!=0) props.addNote(title,text); 
+          if(title.length!=0 && text.length!=0)  props.addNote(title,text); 
           event.preventDefault();
           setTitle(""); 
           setText("");
